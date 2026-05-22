4c08c2803dda2190edd73c00d811fb89ec2a9242
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-05-23 00:02:39 +0200

    feat: install log1x/laravel-webfonts package and add Inter font
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,6 @@ $theme = app(UserSettings::class)->theme;
 
         <title>{{ $title ?? __('Warranty Assistant') }}</title>
 
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-        
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         @livewireStyles
